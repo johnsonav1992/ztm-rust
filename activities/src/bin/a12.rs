a480cb73923765049330a7089eac592ad9a8dfc0
Alex Johnson <johnsonav1992@gmail.com>
2024-03-29 21:51:40 -0500

    a12 impl

--- activities/src/bin/a12.rs
+++ activities/src/bin/a12.rs
@@ -6,8 +6,38 @@
 //
 // Notes:
 // * Use a struct to encapsulate the box characteristics
+struct ShippingBox {
+    dimensions: (f64, f64, f64),
+    weight: f64,
+    color: Color,
+}
 // * Use an enum for the box color
+#[derive(Debug)]
+enum Color {
+    Red,
+    Green,
+    Blue,
+}
 // * Implement functionality on the box struct to create a new box
+impl ShippingBox {
+    fn new(dimensions: (f64, f64, f64), weight: f64, color: Color) -> Self {
+        Self {
+            dimensions,
+            weight,
+            color,
+        }
+    }
+
+    fn print_characteristics(&self) {
+        println!("Dimensions: {:?}", self.dimensions);
+        println!("Weight: {}", self.weight);
+        println!("Color: {:?}", self.color);
+    }
+}
 // * Implement functionality on the box struct to print the characteristics
 
-fn main() {}
+
+fn main() {
+    let shipping_box = ShippingBox::new((10.0, 20.0, 30.0), 50.0, Color::Red);
+    shipping_box.print_characteristics();
+}
