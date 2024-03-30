d523bf766c8c3cedcb3adb2d726170c478de4ce5
Alex Johnson <johnsonav1992@gmail.com>
2024-03-30 06:45:54 -0500

    vectors intro

--- activities/src/bin/a13.rs
+++ activities/src/bin/a13.rs
@@ -10,4 +10,17 @@
 // * Determine whether to print the number or print "thirty" inside the loop
 // * Use the .len() function to print the number of elements in a vector
 
-fn main() {}
+use std::vec;
+
+fn main() {
+    let vec: Vec<i32> = vec![10, 20, 30, 40];
+
+    for elem in vec.iter() {
+        match elem {
+            30 => println!("thirty"),
+            _ => println!("{:?}", elem),
+        }
+    }
+
+    println!("Length: {:?}", vec.len());
+}
